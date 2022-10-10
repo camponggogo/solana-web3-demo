e3fa8a76d3c081f2312f809a7a0ab34907cfeb05
campong <campong@gmail.com>
2022-10-10 22:40:44 +0700

    Update echo program lib.rs

--- advanced/interact-with-program/echo/program/src/lib.rs
+++ advanced/interact-with-program/echo/program/src/lib.rs
@@ -2,13 +2,6 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
-// 這裡就像是program的main
-// 所有program一進來都會先到你指定的這個entrypoint上
-// 在執行時會傳入三個參數
-// program id => 這個就是當前的program的id
-// accounts => 由client傳入，這次操作需要的account，solana在發交易的時候就要把所有需要的accoutn都帶入，無法在program上動態加載
-// instruction_data => 由client傳入，需要的data，會是一個uint8的array，如何切分就要跟client協調好。
-
 // here is like a main function
 // when an instruction executes, it will run into here first.
 // it will also pass three arguments
